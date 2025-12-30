8dae12c9aa64dfc915467daab681fb5863497d4c
homocodian <homocodian@gmail.com>
2025-12-30 20:37:42 +0530

    fix: broken link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,5 @@
 import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
-import {
-	NavigationMenu,
-	NavigationMenuItem,
-	NavigationMenuLink,
-	NavigationMenuList,
-} from "./ui/navigation-menu";
 import { Button } from "./ui/button";
 import { Suspense } from "react";
 import { UserAccount } from "./user-account";
@@ -13,6 +7,7 @@ import { AppConfig } from "@/lib/app-config";
 import { CartCount } from "@/components/cart-count";
 import { links } from "@/constants/links";
 import { HeaderMobileMenu } from "./header-mobile-menu";
+import { cn } from "@/lib/utils";
 
 export type HeaderProps = {
 	showCart?: boolean;
@@ -20,7 +15,12 @@ export type HeaderProps = {
 
 export function Header({ showCart = true }: HeaderProps) {
 	return (
-		<header className="sticky px-4 top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+		<header
+			className={cn(
+				"sticky px-4 top-0 z-40 w-full border-b bg-background/95",
+				"backdrop-blur supports-[backdrop-filter]:bg-background/60"
+			)}
+		>
 			<div className="container mx-auto flex h-16 items-center justify-between">
 				<div className="flex items-center gap-6 md:gap-10">
 					<HeaderMobileMenu />
@@ -30,27 +30,27 @@ export function Header({ showCart = true }: HeaderProps) {
 							{AppConfig.name}
 						</span>
 					</Link>
-					<NavigationMenu className="hidden md:block">
-						<NavigationMenuList>
+					<div className="hidden md:flex">
+						<>
 							{links.map((link) => (
-								<NavigationMenuItem key={link.name}>
-									<NavigationMenuLink
-										asChild
-										className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
-									>
-										<a href={link.href}>
-											<div className="flex items-center">
-												{link.icon ? (
-													<link.icon className="mr-2 h-4 w-4" />
-												) : null}
-												{link.name}
-											</div>
-										</a>
-									</NavigationMenuLink>
-								</NavigationMenuItem>
+								<a
+									key={link.name}
+									href={link.href}
+									className={cn(
+										"group flex h-10 items-center justify-center gap-2",
+										"rounded-md px-4 py-2 text-sm leading-none",
+										"font-medium transition-colors hover:bg-accent",
+										"hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+										"focus:outline-none disabled:pointer-events-none disabled:opacity-50",
+										"data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+									)}
+								>
+									{link.icon && <link.icon className="size-4" />}
+									{link.name}
+								</a>
 							))}
-						</NavigationMenuList>
-					</NavigationMenu>
+						</>
+					</div>
 				</div>
 
 				<div className="flex items-center gap-4">
